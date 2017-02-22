0e5ba59b6897d55f06957395d1a75cefa4854793
Patrick Shuff <patrick.shuff@gmail.com>
2017-02-22 14:51:16 -0800

    Adding support for stdin to grep

--- cpp/grep.cpp
+++ cpp/grep.cpp
@@ -6,11 +6,28 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     string line;
+    istream *in;
     ifstream myfile;
+    if (argc == 1) {
+        cout << "Usage:  grep search_string filename\n";
+        exit(1);
+    }
+
     std::string grep_str = argv[1];
-    std::string fname = argv[2];
-    myfile.open(fname.c_str());
-    if (myfile.is_open()) {
+    if (argc == 2) {
+        cout << "Using stdin";
+        while (getline(std::cin, line)) {
+            std::size_t found = line.find(grep_str);
+            if (found != std::string::npos) {
+                cout << line << "\n";
+            }
+        }
+    }
+
+    // TODO(shuff): Dedup these while loops with pointers
+    if (argc == 3) {
+        std::string fname = argv[2];
+        myfile.open(fname.c_str());
         while (getline(myfile, line)) {
             std::size_t found = line.find(grep_str);
             if (found != std::string::npos) {
